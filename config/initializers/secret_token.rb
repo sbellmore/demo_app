5abd38b699260030b75f01b5456e21bf18e0a62f
sbellmore <shawn.bellmore@ascendlearning.com>
2012-11-13 13:22:22 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '35b6b7e1f9686febf42fc8c0fc84782a0ac32af2b36292ec47751c5b20a8a079da7fdb63687507df5c3f5f806ac6b8f5059efe3acfdeecc16054373f3e47c8f8'
